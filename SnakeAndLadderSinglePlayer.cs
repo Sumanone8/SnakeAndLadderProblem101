6594ba30e19c06011ccd50b9475bf02e6c318a7e
Sumanone8 <sumanjhabhl@gmail.com>
2023-07-10 21:43:37 +0530

    Suman : Player rolls the die to get a number between 1 to 6

--- SnakeAndLadderSinglePlayer.cs
+++ SnakeAndLadderSinglePlayer.cs
@@ -13,7 +13,13 @@ namespace SnakeAndLadderProblem101
             Console.WriteLine("For Single Player");
             int position = 0;
             Console.WriteLine("Current position: " + position);
-         
+            Console.WriteLine("Press any key to roll the dice...");
+            Console.ReadKey();
+
+            Random random = new Random();
+            int diceRoll = random.Next(1, 7);
+            Console.WriteLine("Dice rolled: " + diceRoll);
+
         }
 
     }
