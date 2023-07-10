5e85e9dbae9bd588951375724fb5c7f93d9cbbbb
Sumanone8 <sumanjhabhl@gmail.com>
2023-07-10 22:03:35 +0530

    Suman : Added player's option to check for No Play, Ladder, or Snake

--- SnakeAndLadderSinglePlayer.cs
+++ SnakeAndLadderSinglePlayer.cs
@@ -20,6 +20,28 @@ namespace SnakeAndLadderProblem101
             int diceRoll = random.Next(1, 7);
             Console.WriteLine("Dice rolled: " + diceRoll);
 
+            int option = random.Next(0, 3);
+            switch (option)
+            {
+                case 0:
+                    Console.WriteLine("No Play! You stay at the current position.");
+                    break;
+                case 1:
+                    int ladderSteps = random.Next(1, 7);
+                    Console.WriteLine("Congratulations! You climbed a ladder and move forward by " + ladderSteps + " steps.");
+                    position += ladderSteps;
+                    break;
+                case 2:
+                    int snakeSteps = random.Next(1, 7);
+                    Console.WriteLine("Oh no! You got bitten by a snake and move backward by " + snakeSteps + " steps.");
+                    position -= snakeSteps;
+                    break;
+            }
+
+            // Check if the position is negative
+            if (position < 0)
+                position = 0;
+
         }
 
     }
