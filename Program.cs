5afd372f9bb1dc4745cbdf93b4dde2058a8866a4
Sumanone8 <sumanjhabhl@gmail.com>
2023-07-10 09:03:59 +0530

    Suman : Displaying Welcome Massage.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeAndLadderProblem101
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey Fellow Learners!\nWelcome to Snake and Ladder Problem.");
+        }
+    }
+}
